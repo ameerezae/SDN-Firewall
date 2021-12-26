f3766c4a2a16a19ab2b5aaf5647d9aa6745f7bb9
Amir Rezaei <amir.rezaei1999@yahoo.com>
2021-12-26 20:54:48 +0330

    Add policy file

--- pox/pox/misc/policyController.py
+++ pox/pox/misc/policyController.py
@@ -1,35 +1,32 @@
-# Part 4 of UWCSE's Project 3
-#
-# based on Lab Final from UCSC's Networking Class
-# which is based on of_tutorial by James McCauley
-
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.addresses import IPAddr, IPAddr6, EthAddr
-import pox.lib.packet as pkt
+import os
 
 log = core.getLogger()
+print(os.getcwd())
 
 
-# statically allocate a routing table for hosts
-# MACs used in only in part 4
+
+file = open('./policy.txt')
+line = file.readline().split(' ')
+first_device_ip = line[0]
+first_device_server_port = int(line[1])
+first_device_client_port = int(line[2])
+second_device_ip = line[3]
+second_device_server_port = int(line[4])
+second_device_client_port = int(line[5])
 
 
-class Part4Controller(object):
-    """
-  A Connection object for that switch is passed to the __init__ function.
-  """
+class PolicyController(object):
 
     def __init__(self, connection):
         print(connection.dpid)
-        # Keep track of the connection to the switch so that we can
-        # send it messages!
         self.connection = connection
 
-        # This binds our PacketIn event listener
         connection.addListeners(self)
-        # use the dpid to figure out what switch is being created
-        if (connection.dpid == 1):
+
+        if connection.dpid == 1:
             self._table = {}
             self.allow = False
 
@@ -37,47 +34,37 @@ class Part4Controller(object):
             print("UNKNOWN SWITCH")
             exit(1)
 
-    def _handle_ConnectionUp(self, event):
-        # handle ARP
+    def handle_ARP(self, event):
         fm = of.ofp_flow_mod()
         fm.priority = 33001
         fm.match.dl_type = 0x0806
         fm.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
         event.connection.send(fm)
 
-    def _handle_PacketIn(self, event):
-        """
-    Packets not handled by the router rules will be
-    forwarded to this method to be handled by the controller
-    """
+    def _handle_ConnectionUp(self, event):
+        self.handle_ARP(event)
 
+    def _handle_PacketIn(self, event): # Packets not handled by the router rules will be forwarded to this method to be handled by the controller
         packet = event.parsed  # This is the parsed packet data.
         if not packet.parsed:
             log.warning("Ignoring incomplete packet")
             return
 
-        packet_in = event.ofp  # The actual ofp_packet_in message.
+        # packet_in = event.ofp  # The actual ofp_packet_in message.
 
-        # if self._is_arp(packet):  # handle ARP traffic?
-        #     self._handle_ARP(packet, event)
-        # else:  # learn and forward?
         self._forward_to_switch(packet, event)
 
-        # print('Unhandled packet from ' + str(self.connection.dpid) + ':' + packet.dump())
+        print('Unhandled packet from ' + str(self.connection.dpid) + ':' + packet.dump())
 
-    # learns port/MAC info, if new, otherwise, updates known
-    def _update(self, inport, packet):
+    def _update(self, inport, packet): # learns port/MAC info, if new, otherwise, updates known
         src = packet.next.srcip
         if src in self._table and self._table[src] != (packet.src, inport):
-            print(f"Re-learned {src}")  # update info
+            print(f"Re-learned {src}")
         elif src not in self._table:
-            print(f"Learned {str(src)}")  # new dst to learn
+            print(f"Learned {str(src)}")
         self._table[src] = (packet.src, inport)
-        # print(self._table)
 
-    # forward this packet to its destaination, and add to the flow table
-    def _forward_to_switch(self, p, event):
-        log.debug('HEEEEEE: ' + str(p.next.dstip))
+    def _forward_to_switch(self, p, event):  # forward this packet to its destaination, and add to the flow table
         if not isinstance(p.next.dstip, IPAddr6):
             self._update(event.port, p)  # new knowledge?
         conn = event.connection
@@ -87,7 +74,6 @@ class Part4Controller(object):
             if isinstance(dest, IPAddr6):
                 dest = self._find_by_port(event.port)
             dst = (self._table[dest][-1], self._table[dest][0])  # port, mac
-            # print(type(p.payload.payload.srcport))
 
             if dst[0] == event.port:  # through in-port?
                 log.warning("Not sending packet back out of in-port " + str(event.port))
@@ -95,12 +81,12 @@ class Part4Controller(object):
                 do = [of.ofp_action_dl_addr.set_dst(dst[1]),  # MAC addr of dest
                       of.ofp_action_output(port=dst[0])]  # the port to dest
                 want = of.ofp_match.from_packet(p, event.port)
-                if p.next.srcip == "10.0.0.1" and \
-                        p.payload.payload.srcport == 78 and \
-                        p.next.dstip == "10.0.0.2" and \
-                        p.payload.payload.dstport == 5566 and \
+                if p.next.srcip == first_device_ip and \
+                        p.payload.payload.srcport == first_device_client_port and \
+                        p.next.dstip == second_device_ip and \
+                        p.payload.payload.dstport == second_device_server_port and \
                         self.allow == False:
-                    print('#################################################################3')
+                    print('################################################################# NOT ALLOWED')
                 else:
                     conn.send(of.ofp_flow_mod(command=of.OFPFC_ADD,  # learn new rule
                                               idle_timeout=10,  # from l3learning.py
@@ -108,11 +94,12 @@ class Part4Controller(object):
                                               buffer_id=event.ofp.buffer_id,
                                               actions=do,
                                               match=want))
-                    if p.next.srcip == "10.0.0.2" and \
-                            p.payload.payload.srcport == 78 and \
-                            p.next.dstip == "10.0.0.1" and \
-                            p.payload.payload.dstport == 5566:
+                    if p.next.srcip == second_device_ip and \
+                            p.payload.payload.srcport == second_device_client_port and \
+                            p.next.dstip == first_device_ip and \
+                            p.payload.payload.dstport == first_device_server_port:
                         self.allow = True
+                        print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ ALLOWED')
                     log.info('Added flow rule: traffic to ' + str(dest) + ' via ' + str(dst[0]))
 
             print('{a} forwarded packet from {b}>'.format(a=me, b=p.next.srcip) +
@@ -120,12 +107,9 @@ class Part4Controller(object):
 
 
 def launch():
-    """
-  Starts the component
-  """
 
     def start_switch(event):
         log.debug("Controlling %s" % (event.connection,))
-        Part4Controller(event.connection)
+        PolicyController(event.connection)
 
     core.openflow.addListenerByName("ConnectionUp", start_switch)
